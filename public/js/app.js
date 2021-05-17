19c97974d99b4a14397d9af16e8c65267f44e49c
Gonzalo Celina <gonzalocelina@gmail.com>
2021-05-17 11:46:29 +0100

    package-lock

--- public/js/app.js
+++ public/js/app.js
@@ -49958,8 +49958,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/apple/Sites/three-application/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/apple/Sites/three-application/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/apple/Sites/laravel-stocks/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/apple/Sites/laravel-stocks/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
